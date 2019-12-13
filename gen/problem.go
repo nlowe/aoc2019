57e567a9436af6fc4b38a4d9d9c4e33d235e7f08
Nathan Lowe <public@nlowe.me>
2019-12-12 22:02:59 -0500

    Update problem generator to fetch input

--- gen/problem.go
+++ gen/problem.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"io"
+	"io/ioutil"
+	"net/http"
 	"os"
 	"os/exec"
 	"path/filepath"
@@ -10,6 +12,9 @@ import (
 	"strconv"
 	"strings"
 	"text/template"
+	"time"
+
+	"github.com/zellyn/kooky"
 )
 
 const (
@@ -80,47 +85,111 @@ func main() {
 	}
 
 	m := metadata{N: n, AB: ab}
-
 	funcs := template.FuncMap{
 		"toLower": strings.ToLower,
 	}
 
-	challengeTemplate := template.Must(template.New("challenge").Funcs(funcs).Parse(problemTemplate))
-	cf, err := os.Create(filepath.Join(p, fmt.Sprintf("%s.go", strings.ToLower(ab))))
-	if err != nil {
-		abort(err)
-	}
-
-	defer mustClose(cf)
-	if err := challengeTemplate.Execute(cf, m); err != nil {
-		abort(err)
-	}
-
-	testTemplate := template.Must(template.New("test").Funcs(funcs).Parse(testTemplate))
-	tf, err := os.Create(filepath.Join(p, fmt.Sprintf("%s_test.go", strings.ToLower(ab))))
-	if err != nil {
-		abort(err)
-	}
-
-	defer mustClose(tf)
-	if err := testTemplate.Execute(tf, m); err != nil {
-		abort(err)
-	}
+	genFile(filepath.Join(p, fmt.Sprintf("%s.go", strings.ToLower(ab))), problemTemplate, funcs, m)
+	genFile(filepath.Join(p, fmt.Sprintf("%s_test.go", strings.ToLower(ab))), testTemplate, funcs, m)
 
 	goimports := exec.Command("goimports", "-w", p)
 	if err := goimports.Run(); err != nil {
 		abort(err)
 	}
 
-	input, err := os.OpenFile(filepath.Join(p, "input.txt"), os.O_CREATE|os.O_APPEND, 0644)
-	if err != nil {
-		abort(err)
+	if _, stat := os.Stat(filepath.Join(p, "input.txt")); os.IsNotExist(stat) {
+		fmt.Println("fetching input for day", n)
+		problemInput, err := getInput(n)
+		if err != nil {
+			panic(err)
+		}
+
+		if err := ioutil.WriteFile(filepath.Join(p, "input.txt"), problemInput, 0644); err != nil {
+			panic(err)
+		}
+	} else {
+		fmt.Println("input already downloaded, skipping...")
 	}
-	defer mustClose(input)
 
 	fmt.Printf("Generated problem %s for day %d. Be sure to add it to main.go\n", ab, n)
 
 	// TODO: Can we modify main.go easily?
+}
+
+func genFile(path, t string, funcs template.FuncMap, m metadata) {
+	if _, stat := os.Stat(path); os.IsNotExist(stat) {
+		fmt.Println("creating", path)
+		t := template.Must(template.New(path).Funcs(funcs).Parse(t))
+		cf, err := os.Create(path)
+		if err != nil {
+			abort(err)
+		}
+
+		defer mustClose(cf)
+		if err := t.Execute(cf, m); err != nil {
+			abort(err)
+		}
+	} else {
+		fmt.Println(path, "already exists, skipping...")
+	}
+}
+
+func chromeCookiePath() (string, error) {
+	if p, set := os.LookupEnv("CHROME_PROFILE_PATH"); set {
+		return filepath.Join(p, "Cookies"), nil
+	}
+
+	if runtime.GOOS == "windows" {
+		localAppData, err := os.UserCacheDir()
+		return filepath.Join(localAppData, "Google", "Chrome", "User Data", "Default", "Cookies"), err
+	}
+
+	return "", fmt.Errorf("chrome cookie path for GOOS %s not implemented, set CHROME_PROFILE_PATH instead", runtime.GOOS)
+}
+
+func getInput(day int) ([]byte, error) {
+	cookiePath, err := chromeCookiePath()
+	if err != nil {
+		return nil, err
+	}
+
+	_, _ = os.UserConfigDir()
+	_, _ = os.UserCacheDir()
+	cookies, err := kooky.ReadChromeCookies(cookiePath, ".adventofcode.com", "session", time.Time{})
+	if err != nil {
+		return nil, err
+	}
+
+	if len(cookies) == 0 {
+		return nil, fmt.Errorf("session cookie not found, ensure that you are logged in")
+	}
+
+	req, err := http.NewRequest(http.MethodGet, fmt.Sprintf("https://adventofcode.com/2019/day/%d/input", day), nil)
+	if err != nil {
+		return nil, err
+	}
+
+	sessionToken := cookies[0].HttpCookie()
+	req.AddCookie(&sessionToken)
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer mustClose(resp.Body)
+
+	body, err := ioutil.ReadAll(resp.Body)
+	return body, err
+}
+
+func mustClose(c io.Closer) {
+	if c == nil {
+		return
+	}
+
+	if err := c.Close(); err != nil {
+		panic(fmt.Errorf("error closing io.Closer: %w", err))
+	}
 }
 
 func pkgPath(day int) string {
@@ -135,14 +204,4 @@ func pkgPath(day int) string {
 func abort(err error) {
 	fmt.Printf("%s\n\nsyntax: go run gen/problem.go <day> <a|b>\n", err.Error())
 	os.Exit(1)
-}
-
-func mustClose(c io.Closer) {
-	if c == nil {
-		return
-	}
-
-	if err := c.Close(); err != nil {
-		panic(fmt.Errorf("error closing io.Closer: %w", err))
-	}
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,9 @@ module github.com/nlowe/aoc2019
 go 1.13
 
 require (
+	github.com/go-sqlite/sqlite3 v0.0.0-20180313105335-53dd8e640ee7 // indirect
+	github.com/gonuts/binary v0.2.0 // indirect
+	github.com/keybase/go-keychain v0.0.0-20191114153608-ccd67945d59e // indirect
 	github.com/magiconair/properties v1.8.1
 	github.com/pelletier/go-toml v1.6.0 // indirect
 	github.com/pkg/profile v1.4.0
@@ -12,6 +15,8 @@ require (
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/spf13/viper v1.5.0
 	github.com/stretchr/testify v1.4.0
+	github.com/zalando/go-keyring v0.0.0-20191212171435-ac5f1d08068b // indirect
+	github.com/zellyn/kooky v0.0.0-20190514172626-f2bb24889ec7
 	golang.org/x/sys v0.0.0-20191128015809-6d18c012aee9 // indirect
 	golang.org/x/text v0.3.2 // indirect
 	gopkg.in/yaml.v2 v2.2.7 // indirect
