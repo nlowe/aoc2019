02f38359073025bee538df98cd30ccedc4c14558
Nathan Lowe <public@nlowe.me>
2019-12-30 18:34:00 -0500

    Fix generator import path

--- util/pkg_path.go
+++ util/pkg_path.go
@@ -7,10 +7,10 @@ import (
 )
 
 func PkgPath(day int) (string, error) {
-	_, filename, _, ok := runtime.Caller(1)
+	_, filename, _, ok := runtime.Caller(0)
 	if !ok {
 		return "", fmt.Errorf("failed to generate package path")
 	}
 
-	return filepath.Join(filepath.Dir(filepath.Dir(filename)), fmt.Sprintf("day%d", day)), nil
+	return filepath.Join(filepath.Dir(filepath.Dir(filename)), "challenge", fmt.Sprintf("day%d", day)), nil
 }
